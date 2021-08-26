e38f03033fe6829f4c58af4501ce633cb12691bb
Pablo <cruzpablomartin@gmail.com>
2021-08-26 16:32:59 -0300

    Update notifications

--- src/components/UI/Notification.js
+++ src/components/UI/Notification.js
@@ -1,5 +1,8 @@
+import { Fragment } from 'react';
 import { useParams, useHistory } from 'react-router';
 import classes from './Notification.module.css';
+
+import login from '../../assets/login.jpg';
 
 const Notification = () => {
   const params = useParams();
@@ -7,9 +10,18 @@ const Notification = () => {
 
   return (
     <div className={classes.notification}>
-      <h2>{params.msg}</h2>
-      {params.msg !== 'Token expired...' && <button onClick={() => history.go(-2)}>◀ &#x2063; Regresar</button>}
-      {/* <button onClick={() => closeCurrentWindow()}>Salir</button> */}
+      {params.msg === 'Token expired...'
+        ?
+        <Fragment>
+          <img src={login} alt='logo' />
+          <p><span>El tiempo de uso de la aplicación ha finalizado.</span> <br /> Si desea continuar usando la aplicación, cierre el navegador y vuelva a ingresar.</p>
+        </Fragment>
+        :
+        <Fragment>
+          <h2>{params.msg}</h2>
+          <button onClick={() => history.go(-2)}>◀ &#x2063; Regresar</button>
+        </Fragment>
+      }
     </div>
   );
 }
