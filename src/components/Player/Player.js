2687638b9018666af009253134b5e259953f7476
Pablo <cruzpablomartin@gmail.com>
2021-08-27 19:06:15 -0300

    Update player settings

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -13,20 +13,14 @@ const Player = (props) => {
     <div className={classes.player}>
       <SpotifyPlayer
         token={sessionStorage.getItem('playerToken')}
-        // uris={track.uri ? [track.uri] : []}
-
-        uris={props.track}
-
+        uris={props.track} // uris={uri or [uris]}
         initialVolume={0.5}
         autoPlay={true}
-
         play={play}
         // play={true}
-
         callback={state => {
           if (!state.isPlaying) setPlay(false);
         }}
-
         styles={{
           activeColor: '#fff',
           bgColor: '#212121',
@@ -37,12 +31,8 @@ const Player = (props) => {
           trackNameColor: '#fff',
           sliderTrackColor: '#909090',
           sliderHandleColor: '#f00',
-          height: '7rem',
-          // altColor: ,
-          // sliderHeight: ,
+          height: '6rem',
         }}
-
-      // offset={5}, Número de pista en las que desea iniciar el reproductor.
       />
     </div>
   );
